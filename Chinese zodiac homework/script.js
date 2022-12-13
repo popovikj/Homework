053eb5fd6ba1589cf44f04d22bebfac8bb3d83d9
popovikj <120363520+popovikj@users.noreply.github.com>
2022-12-14 00:42:12 +0100

    Add files via upload

---
+++ Chinese zodiac homework/script.js
@@ -0,0 +1,42 @@
+// console.log("connected")
+
+let input = prompt("Enter date of bith");
+let numberInput = parseInt(input);
+
+
+
+let year = 2030;
+
+
+if ((year - 4) % 12 === 0) {
+    alert("Rat"); console.log("Rat")
+} else if ((year - 4) % 12 === 1) {
+    alert("Ox"); console.log("Ox")
+} else if ((year - 4) % 12 === 2) {
+    alert("Tiger"); console.log("Tiger")
+} else if ((year - 4) % 12 === 3) {
+    alert("Rabbit"); console.log("Rabbit")
+} else if ((year - 4) % 12 === 4) {
+    alert("Dragon"); console.log("Dragon")
+} else if ((year - 4) % 12 === 5) {
+    alert("Snake"); console.log("Snake")
+} else if ((year - 4) % 12 === 6) {
+    alert("Horse"); console.log("Horse")
+} else if ((year - 4) % 12 === 7) {
+    alert("Goat"); console.log("Goat")
+} else if ((year - 4) % 12 === 8) {
+    alert("Monkey"); console.log("Monkey")
+} else if ((year - 4) % 12 === 9) {
+    alert("Rooster"); console.log("Rooster")
+} else if ((year - 4) % 12 === 10) {
+    alert("Dog"); console.log("Dog")
+} else if ((year - 4) % 12 === 11) {
+    alert("Pig"); console.log("Pig")
+
+}
+
+
+
+
+
+
